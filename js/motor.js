a7f56eebb688e93fcfa4dad36cbbbe056396b797
Cristian <cristian.carre.online@gmail.com>
2022-01-03 20:07:14 +0100

    added more sounds

--- js/motor.js
+++ js/motor.js
@@ -3,39 +3,63 @@ var estadoCaja= "close";
 var num;
 var pista;
 var sonido;
-
+var block=false;
 
 function abrirCaja(){
-    if (estadoCaja=="close"){
+ if(block==false){
+    console.log("intento abrir");
+
+     if (estadoCaja=="close"){
+       
         num=Math.floor(Math.random()*6);
         laSorpresa();
-        estadoCaja="open";
+      
         caja.classList.add("abierta");//añade a la classe más texto y asi cambia el nombre en css
         console.log("se abrió");
        
-    }else{
-        laSorpresa();
-        estadoCaja="close";
+      }else{
+        
+        laSorpresa(); 
         caja.classList.remove("abierta");
         console.log("se cerró");
         
-    }
-}
+      }
+   }else{
+    console.log("espera a que acabe la animación");
+     }
+   }
+
 
  function laSorpresa(){
+    console.log("entro en sorpresa");
+     block=true;
     if (estadoCaja=="close"){
         reproducir(6);
+        console.log("haré aleatorioGif");
         aleatorioGif(num); 
+        estadoCaja="open";
     }else{
         reproducir(7);
+        console.log("Si la voy a cerrar no puedo hacer aleatorioGif");
+        estadoCaja="close";
+        block=false
     }
 }
 
 
  function reproducir(pista){
 
+    console.log("empieza a sonar "+pista);
     sonido =document.getElementById("sound."+ pista.toString());
     sonido.play();
+    
+if(pista<6){
+    console.log("la pista no es ni 6 ni 7 es "+pista);
+    sonido.addEventListener("ended", function(){
+        block=false
+        console.log("fin de la canción "+pista);})
+    }
+    
 }
 
     
@@ -46,8 +70,7 @@ function aleatorioGif(num){
      reproducir(num);
 
     break;
-    case 1:
-        console.log("entro en Switch");
+    case 1:        
         reproducir(num);
 
     break;
