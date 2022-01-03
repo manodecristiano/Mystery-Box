260c5c823a26bac021007826ae41ab5e7826861c
Cristian <cristian.carre.online@gmail.com>
2022-01-03 14:37:28 +0100

    solved problem with the sounds

--- js/motor.js
+++ js/motor.js
@@ -1,32 +1,73 @@
 
 var estadoCaja= "close";
-var cajaRoja =document.getElementById("caja");
-var sonidoAleatorio ="none";
-var sonidoAbrir =document.getElementById("abrirCajaSound");
-var sonidoCerrar =document.getElementById("cerrarCajaSound");
+var num;
+var pista;
+var sonido;
+
 
 function abrirCaja(){
     if (estadoCaja=="close"){
+        num=Math.floor(Math.random()*6);
+        laSorpresa();
         estadoCaja="open";
         caja.classList.add("abierta");//añade a la classe más texto y asi cambia el nombre en css
         console.log("se abrió");
-        laSorpresa();
+       
     }else{
+        laSorpresa();
         estadoCaja="close";
         caja.classList.remove("abierta");
         console.log("se cerró");
-        laSorpresa();
+        
     }
+}
 
  function laSorpresa(){
     if (estadoCaja=="close"){
-        
-        sonidoAbrir.play();
+        reproducir(6);
+        aleatorioGif(num); 
     }else{
-       
-        sonidoCerrar.play();
+        reproducir(7);
     }
-
-
- }   
 }
+
+
+ function reproducir(pista){
+
+    sonido =document.getElementById("sound."+ pista.toString());
+    sonido.play();
+}
+
+    
+function aleatorioGif(num){
+    console.log("el random="+num);
+  switch(num){
+    case 0:
+     reproducir(num);
+
+    break;
+    case 1:
+        console.log("entro en Switch");
+        reproducir(num);
+
+    break;
+    case 2:
+        reproducir(num);
+
+    break;
+    case 3:
+        reproducir(num);
+
+    break;
+    case 4:
+        reproducir(num);
+
+    break;
+    case 5:
+        reproducir(num);
+
+    break;
+
+    }
+}
+
