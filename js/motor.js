d602dfbb2553b3f0edc962e7b81e1846bd610a04
Cristian <cristian.carre.online@gmail.com>
2022-01-04 12:09:07 +0100

    finaly add a animation border and gifs

--- js/motor.js
+++ js/motor.js
@@ -3,7 +3,12 @@ var estadoCaja= "close";
 var num;
 var pista;
 var sonido;
+var nameGif;
 var block=false;
+var imagen=document.getElementById("imagen");
+var caja=document.getElementById("caja");
+var fondo=document.getElementById("container");
+var border=document.getElementById("bb");
 
 function abrirCaja(){
  if(block==false){
@@ -14,7 +19,7 @@ function abrirCaja(){
         num=Math.floor(Math.random()*6);
         laSorpresa();
       
-        caja.classList.add("abierta");//añade a la classe más texto y asi cambia el nombre en css
+        caja.classList.add("abierta");//añade a la clase más texto y asi cambia el nombre en css
         console.log("se abrió");
        
       }else{
@@ -36,6 +41,7 @@ function abrirCaja(){
     if (estadoCaja=="close"){
         reproducir(6);
         console.log("haré aleatorioGif");
+        border.classList.add("none")
         aleatorioGif(num); 
         estadoCaja="open";
     }else{
@@ -47,47 +53,62 @@ function abrirCaja(){
 }
 
 
- function reproducir(pista){
-
+ function reproducir(pista, nameGif,color){
+    console.log("nameGif= "+nameGif);
     console.log("empieza a sonar "+pista);
-    sonido =document.getElementById("sound."+ pista.toString());
+    sonido= document.getElementById("sound."+ pista.toString());
     sonido.play();
-    
+    imagen.classList.add(nameGif);
+    fondo.classList.add(color);
+    ;
 if(pista<6){
     console.log("la pista no es ni 6 ni 7 es "+pista);
     sonido.addEventListener("ended", function(){
         block=false
+        imagen.classList.remove(nameGif);
+        fondo.classList.remove(color);
+        border.classList.remove("none");
         console.log("fin de la canción "+pista);})
     }
     
 }
 
+
     
 function aleatorioGif(num){
-    console.log("el random="+num);
+    
   switch(num){
     case 0:
-     reproducir(num);
+    
+    reproducir(num,"calavera","black");
 
     break;
-    case 1:        
-        reproducir(num);
+    case 1:     
+      
+    reproducir(num,"payaso","red");
 
     break;
     case 2:
-        reproducir(num);
+        
+    reproducir(num,"aleluya","blanco");
 
     break;
     case 3:
-        reproducir(num);
+       
+     reproducir(num,"leon","blue");
+
 
     break;
     case 4:
-        reproducir(num);
+        
+    reproducir(num,"mariachi","blanco");
+        
 
     break;
     case 5:
-        reproducir(num);
+       
+     reproducir(num,"mario","blanco");
+
 
     break;
 
